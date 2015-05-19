f7b54e89d715ce0d5e1ee8883a9a8493c798f330
Ovidiu Negus <neguso@hotmail.com>
2015-05-19 21:57:50 +0300

    Added _references.js
    Don't forget to add "~/www/js/_references.js" to Options -> Text Editor
    -> JavaScript -> IntelliSense -> References

---
+++ www/js/_references.js
@@ -0,0 +1,9 @@
+﻿/// <autosync enabled="true" />
+
+/// <reference path="~/www/lib/ionic/ionic.bundle.js" />
+
+/// <reference path="app.js" />
+/// <reference path="help.js" />
+/// <reference path="login.js" />
+/// <reference path="services.js" />
+/// <reference path="start.js" />
